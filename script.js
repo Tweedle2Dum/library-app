51d100c83afa9e59c305d79d3a2a0fb4085db79f
Tweedle2Dum <utkarsh.work1405@gmail.com>
2023-01-10 15:03:10 +0530

    added a working modal

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const openModal = document.querySelector(".login");
+const modal = document.querySelector("#modal")
+const openModal = document.querySelector(".add-book");
 const closeModal = document.querySelector(".button[type=submit]");
 
-console.log(openModal)
+openModal.addEventListener("click",()=>{
+    modal.showModal();
+})
+
+
+closeModal.addEventListener("click",()=>{
+    modal.closeModal();
+})
