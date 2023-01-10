66e45b015cae9c46e069d25495c3a6b3cf9b458d
Tweedle2Dum <utkarsh.work1405@gmail.com>
2023-01-10 15:55:56 +0530

    changed some css props.

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const modal = document.querySelector("#modal")
 const openModal = document.querySelector(".add-book");
-const closeModal = document.querySelector(".button[type=submit]");
 
 openModal.addEventListener("click",()=>{
     modal.showModal();
 })
 
 
-closeModal.addEventListener("click",()=>{
-    modal.closeModal();
-})
