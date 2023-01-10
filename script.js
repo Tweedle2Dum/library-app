4b06df8e9eb41d2b875f7cb2d29c49c3d0a68efc
Tweedle2Dum <utkarsh.work1405@gmail.com>
2023-01-10 14:39:35 +0530

    added the dialog box and started working on js file

--- script.js
+++ script.js
@@ -1,39 +1,4 @@
-let myLibrary = [
+const openModal = document.querySelector(".login");
+const closeModal = document.querySelector(".button[type=submit]");
 
-    {
-        author:"meow",
-        title:"woof",
-        pages:123,
-        read:true  
-    },
-    {
-        author:"hello",
-        title:"world",
-        pages:234,
-        read:false  
-    }
-];
-
-const Book = function(author,title,pages,read){
-
-
-
-    return {
-        author,
-        title,
-        pages,
-        read
-
-    }
-}
-
-
-
-
-function addBookToLibrary(){
-
-    myLibrary.append(Book)
-}
-
-
-
+console.log(openModal)
